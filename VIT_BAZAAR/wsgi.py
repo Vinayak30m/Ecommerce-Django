a16bc591e0c1541c1dddcb169ab698ee3db3d0ff
Vinayak30m <116912446+Vinayak30m@users.noreply.github.com>
2023-03-11 00:17:12 +0530

    All files added

---
+++ VIT_BAZAAR/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for VIT_BAZAAR project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VIT_BAZAAR.settings')
+
+application = get_wsgi_application()
